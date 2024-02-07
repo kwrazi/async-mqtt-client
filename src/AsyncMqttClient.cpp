4122338b561d76017bb6cebc43651a9b247fa878
Ryan Krensel <ryan.krensel@gmail.com>
2024-02-07 12:50:41 +1100

    debugging

--- src/AsyncMqttClient.cpp
+++ src/AsyncMqttClient.cpp
@@ -481,7 +481,7 @@ void AsyncMqttClient::_handleQueue() {
       static auto last = millis();
       auto now = millis();
       
-      if (realSend == 0) {
+      if (true) {
         ESP_LOGE(TAG.data(), "tcp.queue failed, %u, %d-%d=%d", _client.space(),now,last,now-last);
         if(now-last>TCP_FAIL_TIMEOUT) {
           ESP_LOGE(TAG.data(), "TCP timeout. Restarting");
